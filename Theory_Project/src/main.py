02c8df8ea881da55a873b5e0296ecab5ac6a904b
Robinson Merillat <robinson.merillat@gmail.com>
2020-11-01 16:36:54 -0700

    setup torch

---
+++ Theory_Project/src/main.py
@@ -0,0 +1,56 @@
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch.autograd import Variable
+from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from torch.optim import Adam, SGD
+
+import numpy as np
+import pandas as pd
+
+# for reading and displaying images
+from skimage.io import imread
+import matplotlib.pyplot as plt
+
+
+
+# loading dataset
+train = pd.read_csv('train_LbELtWX/train.csv')
+test = pd.read_csv('test_ScVgIM0/test.csv')
+
+sample_submission = pd.read_csv('sample_submission_I5njJSF.csv')
+
+train.head()
+
+
+
+
+# class Net(Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+
+#         self.cnn_layers = Sequential(
+#             # Defining a 2D convolution layer
+#             Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+#             BatchNorm2d(4),
+#             ReLU(inplace=True),
+#             MaxPool2d(kernel_size=2, stride=2),
+#             # Defining another 2D convolution layer
+#             Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
+#             BatchNorm2d(4),
+#             ReLU(inplace=True),
+#             MaxPool2d(kernel_size=2, stride=2),
+#         )
+
+#         self.linear_layers = Sequential(
+#             Linear(4 * 7 * 7, 10)
+#         )
+
+#     # Defining the forward pass
+#     def forward(self, x):
+#         x = self.cnn_layers(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.linear_layers(x)
+#         return x
